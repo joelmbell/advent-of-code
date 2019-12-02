15be00bfe26ea65a9cd1087525a01d038aadba9f
Joel Bell <joel.bell@sap.com>
2019-12-02 08:58:25 -0800

    Day2-Part1 Swift

---
+++ 2019/swift/playground.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+struct FileLoader {
+    enum Err: Error {
+        case loadError
+    }
+
+    func loadFile(named name: String, ext: String = "txt") throws -> [Int] {
+        guard
+            let filePath = Bundle.main.path(forResource: name, ofType: ext),
+            let contentData = FileManager.default.contents(atPath: filePath),
+            let contents = String(data: contentData, encoding: .utf8)
+            else { throw Err.loadError }
+
+        return contents
+            .components(separatedBy: ",")
+            .compactMap(Int.init)
+    }
+}
+
+/**
+ Requirements
+
+ opcode 1 -
+    adds next two, stores result in 3rd (pointers)
+ opcode 2 -
+    multiplies next two stores result in 3rd (pointers)
+ opcode 99 - end program
+ */
+
+
+struct Interpreter {
+    func interprete(_ input: inout [Int]) {
+
+        var i: Int = 0
+        while input[i] != 99, i < input.count {
+            switch input[i] {
+            case 1:
+                code1(i, input: &input)
+                i += 4
+            case 2:
+                code2(i, input: &input)
+                i += 4
+            default: continue
+            }
+        }
+    }
+
+    private func code1(_ i: Int, input: inout [Int]) {
+        let result = input[input[i+1]] + input[input[i+2]]
+        input[input[i+3]] = result
+    }
+
+    private func code2(_ i: Int, input: inout [Int]) {
+        let result = input[input[i+1]] * input[input[i+2]]
+        input[input[i+3]] = result
+    }
+}
+
+
+
+
+
+func solvePart1() -> Int {
+    var fileInput = try! FileLoader().loadFile(named: "input")
+    fileInput[1] = 12
+    fileInput[2] = 2
+    Interpreter().interprete(&fileInput)
+    return fileInput[0]
+}
+
+print("Part 1 solution: \(solvePart1())")
+
+
+
+
+
+
+
+
+
