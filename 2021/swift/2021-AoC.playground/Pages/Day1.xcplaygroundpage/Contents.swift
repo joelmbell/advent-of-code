ae15c614267ebac9f1e55a6abf7fa19ab26af0cd
Joel Bell <joel.bell@sap.com>
2021-12-01 16:19:37 -0800

    2021 Swift Day1-Pt2

--- 2021/swift/2021-AoC.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ 2021/swift/2021-AoC.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -2,26 +2,43 @@
 
 import Foundation
 
-
-func countLarger(input: [Int]) -> Int {
-    var previousValue: Int? = nil
-    var counter: Int = 0
-    for item in input {
-        if let prev = previousValue {
-            if item > prev {
-                counter += 1    
-            }
-        }
-        previousValue = item
-    }
-    return counter
-}
+let data = try! FileLoader().loadFile(named: "pt1Input")
+    .compactMap { Int($0) }
 
 func pt1Solution() {
-    let data = try! FileLoader().loadFile(named: "pt1Input")
-        .compactMap { Int($0) }
+    func countLarger(input: [Int]) -> Int {
+        var previousValue: Int? = nil
+        var counter: Int = 0
+        for item in input {
+            if let prev = previousValue {
+                if item > prev {
+                    counter += 1    
+                }
+            }
+            previousValue = item
+        }
+        return counter
+    }
+    
     print(countLarger(input: data))
 }
 pt1Solution()
 
-//: [Next](@next)
+
+func pt2Solution(input: [Int] = data) -> Int {
+    var counter = 0
+    var idx = 0
+    while idx + 3 < input.count {
+        let current = input[idx] + input[idx+1] + input[idx+2]
+        let next = input[idx+1] + input[idx+2] + input[idx+3]
+        if next > current {
+            counter += 1
+        }
+        idx += 1
+    }
+    return counter
+}
+
+// Testcase
+pt2Solution(input: [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 5
+pt2Solution()
