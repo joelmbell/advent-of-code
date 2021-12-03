d279314d6b2cea0521c7ef0dd636d3c04fce18ec
Joel Bell <joel.m.bell@gmail.com>
2021-12-03 10:12:44 -0800

    2021 Swift Day3-Pt1

---
+++ 2021/swift/2021-AoC.playground/Pages/Day3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,54 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let sampleData = try! FileLoader().loadFile(named: "sample")
+let data = try! FileLoader().loadFile(named: "input")
+
+func convertToDecimal(_ input: [Bool]) -> Int {
+    var counter: Int = 0
+    for (idx, val) in input.reversed().enumerated() {
+        if val {
+            counter += Int(pow(Double(2), Double(idx)))
+        }
+    }
+    return counter
+}
+
+func solveDay1(data: [String]) -> Int {
+    let data = data
+        .filter { !$0.isEmpty }
+        .map { $0.map { Character("1") == $0 } }
+
+    var results: [(on: Int, off: Int)] = []
+    for i in 0..<data[0].count {
+        var countOn = 0
+        var countOff = 0
+        for k in 0..<data.count {
+            if data[k][i] {
+                countOn += 1
+            } else {
+                countOff += 1
+            }
+        }
+        results.append((countOn, countOff))
+    }
+
+    var gamma: [Bool] = []
+    var epsilon: [Bool] = []
+    for result in results {
+        if result.on == result.off {
+            fatalError("Same results")
+        }
+
+        gamma.append(result.on > result.off)
+        epsilon.append(result.on < result.off)
+    }
+
+    return convertToDecimal(epsilon) * convertToDecimal(gamma)
+}
+
+Assert(solveDay1(data: sampleData), 198)
+Assert(solveDay1(data: data), 4138664)
+
+//: [Next](@next)
